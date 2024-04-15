411c9e17619b05ec972792baee5eec84a0c8eeb9
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-04-14 20:33:30 -0600

    Added script file for upgrade oh-my-zsh, pip, npm and homebrew

---
+++ scripts/upgrade.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+# Upgrade Oh My Zsh
+zsh ~/.oh-my-zsh/tools/upgrade.sh
+# Upgrade Home brew
+echo "-----------------------"
+echo "Upgrade Home brew"
+echo "-----------------------"
+brew upgrade `brew outdated` && brew cleanup && brew autoremove
+# Upgrade the pip
+echo "-----------------------"
+echo "Upgrade the pip"
+echo "-----------------------"
+pip3 list --outdated --format json | python3 -m json.tool | grep '.*"name": "\S*"' | cut -d '"' -f4 | xargs -n1 pip3 install -U
+# Upgrade the npm
+echo "-----------------------"
+echo "Upgrade npm"
+echo "-----------------------"
+ncu -g
+`ncu -g | grep "npm -g install .*"`
