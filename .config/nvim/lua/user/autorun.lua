f52700212255b432dd509f9f74ee700b293c9cd8
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-05-22 10:43:22 -0600

    [Updated] name for autorun function and added coments for every language

--- .config/nvim/lua/user/autorun.lua
+++ .config/nvim/lua/user/autorun.lua
@@ -1,20 +1,27 @@
-local function setup_python_execution()
-  vim.api.nvim_command("augroup exe_code")
-  vim.api.nvim_command("autocmd!")
-  vim.api.nvim_command(
-    "autocmd Filetype python nnoremap <buffer> <localleader>r :vs<CR>:term python3 %<CR>:startinsert<CR>"
-  )
-  vim.api.nvim_command(
-    "autocmd Filetype javascript nnoremap <buffer> <localleader>r :vs<CR>:term node %<CR>:startinsert<CR>"
-  )
-  vim.api.nvim_command(
-    "autocmd Filetype sh nnoremap <buffer> <localleader>r :vs<CR>:term bash %<CR>:startinsert<CR>")
-  vim.api.nvim_command(
-    "autocmd Filetype rust nnoremap <buffer> <localleader>r :vs<CR>:term rustc % && ./main<CR>:startinsert<CR>")
-  vim.api.nvim_command(
-    "autocmd Filetype cpp nnoremap <buffer> <localleader>r :vs<CR>:term g++ % -o main && ./main<CR>:startinsert<CR>")
-  vim.api.nvim_command("augroup END")
+local function setup_autorun_execution()
+	vim.api.nvim_command("augroup exe_code")
+	vim.api.nvim_command("autocmd!")
+
+	-- Python3
+	vim.api.nvim_command(
+		"autocmd Filetype python nnoremap <buffer> <localleader>r :vs<CR>:term python3 %<CR>:startinsert<CR>"
+	)
+	-- JavaScript
+	vim.api.nvim_command(
+		"autocmd Filetype javascript nnoremap <buffer> <localleader>r :vs<CR>:term node %<CR>:startinsert<CR>"
+	)
+	-- Bash
+	vim.api.nvim_command(
+		"autocmd Filetype sh nnoremap <buffer> <localleader>r :vs<CR>:term bash %<CR>:startinsert<CR>")
+	-- Rust
+	vim.api.nvim_command(
+		"autocmd Filetype rust nnoremap <buffer> <localleader>r :vs<CR>:term rustc % && ./main<CR>:startinsert<CR>")
+	-- C++
+	vim.api.nvim_command(
+		"autocmd Filetype cpp nnoremap <buffer> <localleader>r :vs<CR>:term g++ % -o main && ./main<CR>:startinsert<CR>")
+
+	vim.api.nvim_command("augroup END")
 end
 
 -- Call the function to set up the autocmds
-setup_python_execution()
+setup_autorun_execution()
