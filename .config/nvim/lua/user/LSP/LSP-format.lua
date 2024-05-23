888661087e19326e9f026e7244445276deabcd5e
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-05-23 10:38:22 -0600

    [Updated] config for lsp

--- .config/nvim/lua/user/LSP/LSP-format.lua
+++ .config/nvim/lua/user/LSP/LSP-format.lua
@@ -4,41 +4,41 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 local lspconfig = require("lspconfig")
 
 local servers = {
-  tsserver = {},
-  biome = {},
-  emmet_ls = {},
-  html = {},
-  cssls = {},
-  tailwindcss = {},
-  astro = {},
-  lua_ls = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" }
-        }
-      }
-    }
-  },
-  pylsp = {},
-  clangd = {},
-  bashls = {},
-  texlab = {},
-  marksman = {},
-  rust_analyzer = {},
-  intelephense = {},
+	tsserver = {},
+	biome = {},
+	emmet_ls = {},
+	html = {},
+	cssls = {},
+	tailwindcss = {},
+	astro = {},
+	lua_ls = {
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" }
+				}
+			}
+		}
+	},
+	pylsp = {},
+	clangd = {},
+	bashls = {},
+	texlab = {},
+	marksman = {},
+	rust_analyzer = {},
+	intelephense = {},
 }
 
 for server, config in pairs(servers) do
-  config.on_attach = function(client)
-    lsp_format.on_attach(client)
-  end
-  config.capabilities = capabilities
-  lspconfig[server].setup(config)
+	config.on_attach = function(client)
+		lsp_format.on_attach(client)
+	end
+	config.capabilities = capabilities
+	lspconfig[server].setup(config)
 end
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
