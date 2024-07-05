09209c48c082f4f8a6a8ecefa479e836037c60eb
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-07-05 10:52:55 -0600

    [New] version for node in fnm "20.15.0" lts and remove ncu from upgrade.sh script

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -13,8 +13,8 @@ echo "Upgrade the pip"
 echo "-----------------------"
 pip3 list --outdated --format json | python3 -m json.tool | grep '.*"name": "\S*"' | cut -d '"' -f4 | xargs -n1 pip3 install -U
 # Upgrade the npm
-echo "-----------------------"
-echo "Upgrade npm"
-echo "-----------------------"
-ncu -g
-`ncu -g | grep "npm -g install .*"`
+# echo "-----------------------"
+# echo "Upgrade npm"
+# echo "-----------------------"
+# ncu -g
+# `ncu -g | grep "npm -g install .*"`
