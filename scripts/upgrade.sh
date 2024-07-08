18880e3ede71fd1862806813e2f87a70601400cb
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-07-08 09:36:16 -0600

    [New] command in upgraade.sh script for npm -g upgrade global packages

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -13,8 +13,9 @@ brew upgrade `brew outdated` && brew cleanup && brew autoremove
 # echo "-----------------------"
 # pip3 list --outdated --format json | python3 -m json.tool | grep '.*"name": "\S*"' | cut -d '"' -f4 | xargs -n1 pip3 install -U
 # Upgrade the npm
-# echo "-----------------------"
-# echo "Upgrade npm"
-# echo "-----------------------"
+echo "-----------------------"
+echo "Upgrade npm"
+echo "-----------------------"
+npm -g upgrade
 # ncu -g
 # `ncu -g | grep "npm -g install .*"`
