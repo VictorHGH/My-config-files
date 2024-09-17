c32308047195afe5cf05248a4b9c95649f221cbc
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-09-17 13:55:01 -0600

    [Updated] script move_windows.sh to work in arch

--- move_windows.sh
+++ move_windows.sh
@@ -5,32 +5,14 @@ link2="path-to-location"
 
 # Function to move window to second monitor for Linux
 move_to_second_monitor_linux() {
-  # Abrir dos instancias de Chromium usando dmenu y diferentes URLs
-  chromium_pid_1=$(dmenu_run -p "Abrir $link1 en Chromium" & flatpak run org.chromium.Chromium "$link1" & echo $!)
-  sleep 1
-  chromium_pid_2=$(dmenu_run -p "Abrir $link2 en Chromium" & flatpak run org.chromium.Chromium --new-window "$link2" & echo $!)
-  sleep 1
+	# Abriendo chromium
+	chromium --new-window $link1
 
-  # Abrir Warp usando dmenu
-  warp_pid=$(dmenu_run -p "Abrir Warp" & flatpak run com.warp & echo $!)
-  sleep 1
+	# Moviendo al segundo monitor
+	chromium --new-window $link2
 
-  # Obtener los IDs de las ventanas de Chromium y Warp usando xdotool
-  chromium_win_1=$(xdotool search --sync --pid $chromium_pid_1 | head -n 1)
-  chromium_win_2=$(xdotool search --sync --pid $chromium_pid_2 | head -n 1)
-  warp_win=$(xdotool search --sync --pid $warp_pid | head -n 1)
-
-  # Mover y cambiar el tamaño de la primera ventana de Chromium a la segunda mitad del primer monitor
-  xdotool windowmove "$chromium_win_1" 940 -20
-  xdotool windowsize "$chromium_win_1" 995 1125
-
-  # Mover y cambiar el tamaño de la segunda ventana de Chromium al monitor completo del segundo monitor
-  xdotool windowmove "$chromium_win_2" 1950 -20
-  xdotool windowsize "$chromium_win_2" 1950 1125
-
-  # Mover y cambiar el tamaño de la ventana de Warp a la primera mitad del primer monitor
-  xdotool windowmove "$warp_win" 0 0
-  xdotool windowsize "$warp_win" 960 1100
+	# Movemos el cursor al segundo monitor
+	xdotool key super+2 
 }
 
 # Function to move window to second monitor for macOS
