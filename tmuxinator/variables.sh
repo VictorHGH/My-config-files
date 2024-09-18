620495578931aa8936b5245cc4ce2944afdc8b57
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-09-18 11:53:56 -0600

    [Fixed] vaariables.sh script

--- tmuxinator/variables.sh
+++ tmuxinator/variables.sh
@@ -3,16 +3,13 @@
 # Detect the operating system
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
-	link1="localhost:8000"
-	link2="https://www.youtube.com/watch?v=3HmFLO29RPU&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=12"
+	link1="view"
+	link2="tutorial"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux
-	link1="localhost/youtube_course"
-	link2="https://www.youtube.com/watch?v=3HmFLO29RPU&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=12"
+	link1="view"
+	link2="tutorial"
 else
     echo "Unsupported operating system"
     exit 1
 fi
-
-export link1, link2
-
