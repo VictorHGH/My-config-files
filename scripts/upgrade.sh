23c6eebee543fe58847820ec3625147a57b8b70d
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-10-03 15:01:49 -0600

    [Updated] upgrade.sh script for use pacman in linux

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,20 +2,32 @@
 
 # Upgrade Oh My Zsh
 zsh ~/.oh-my-zsh/tools/upgrade.sh
+
 # Upgrade Home brew
 echo "-----------------------"
 echo "Upgrade Home brew"
 echo "-----------------------"
 brew upgrade `brew outdated` && brew cleanup && brew autoremove
+
 # Upgrade the pip
 # echo "-----------------------"
 # echo "Upgrade the pip"
 # echo "-----------------------"
 # pip3 list --outdated --format json | python3 -m json.tool | grep '.*"name": "\S*"' | cut -d '"' -f4 | xargs -n1 pip3 install -U
+
 # Upgrade the npm
 echo "-----------------------"
 echo "Upgrade npm"
 echo "-----------------------"
 npm -g update && npm -g upgrade
-# ncu -g
-# `ncu -g | grep "npm -g install .*"`
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Upgrade pacman
+	echo "-----------------------"
+	echo "Upgrade pacman"
+	echo "-----------------------"
+	sudo pacman -Syu --noconfirm
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
