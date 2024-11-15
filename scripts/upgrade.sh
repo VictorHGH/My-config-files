eded45dcb59605f35819f4b11049c350ff375f11
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-11-14 23:53:37 -0600

    [Updated] upgrade.sh script for better organization

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,41 +1,43 @@
 #!/usr/bin/env zsh
 
-# Upgrade Oh My Zsh
-zsh ~/.oh-my-zsh/tools/upgrade.sh
+function print_help() {
+	echo "-----------------------"
+	echo "Upgrade $1"
+	echo "-----------------------"
+}
+function upgrade() {
+	# Upgrade Oh My Zsh
+	print_help "Oh My Zsh"
+	zsh ~/.oh-my-zsh/tools/upgrade.sh
 
-# Upgrade Home brew
-echo "-----------------------"
-echo "Upgrade Home brew"
-echo "-----------------------"
-brew upgrade `brew outdated` && brew cleanup && brew autoremove
+	# Upgrade Home brew
+	print_help "Home brew"
+	brew upgrade `brew outdated` && brew cleanup && brew autoremove
 
-# Upgrade the pip
-# echo "-----------------------"
-# echo "Upgrade the pip"
-# echo "-----------------------"
-# pip3 list --outdated --format json | python3 -m json.tool | grep '.*"name": "\S*"' | cut -d '"' -f4 | xargs -n1 pip3 install -U
+	# Upgrade the npm
+	print_help "npm"
+	npm -g update && npm -g upgrade
 
-# Upgrade the npm
-echo "-----------------------"
-echo "Upgrade npm"
-echo "-----------------------"
-npm -g update && npm -g upgrade
+	# Upgrade the Nvim
+	print_help "Nvim"
+	nvim . -c UpgradeNvim
+
+	# Finish
+	print_help "Finished"
+}
+
+function upgrade_linux() {
+	# Upgrade pacman
+	print_help "pacman"
+	sudo pacman -Syu --noconfirm
+}
 
 # Detect the operating system
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
-    echo "-----------------------"
-	echo "Upgrade finished"
-	echo "-----------------------"
+	upgrade	
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Upgrade pacman
-	echo "-----------------------"
-	echo "Upgrade pacman"
-	echo "-----------------------"
-	sudo pacman -Syu --noconfirm
-    echo "-----------------------"
-	echo "Upgrade finished"
-	echo "-----------------------"
+	upgrade_linux
+	upgrade
 else
     echo "Unsupported operating system"
     exit 1
