003f6785573d7899d74b5f545329089fceabf20c
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-02-28 12:51:58 -0600

    [Updated] plugin for markdown preview

--- global/.config/nvim/lua/user/plugins.lua
+++ global/.config/nvim/lua/user/plugins.lua
@@ -90,6 +90,17 @@ local plugins = {
 	-- SQL UI for Neovim
 	'tpope/vim-dadbod',
 	'kristijanhusak/vim-dadbod-ui',
+
+	-- Markdown preview
+	{
+	  "iamcco/markdown-preview.nvim",
+	  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	  build = "cd app && yarn install",
+	  init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	  end,
+	  ft = { "markdown" },
+	},
 }
 
 local opts = {}
