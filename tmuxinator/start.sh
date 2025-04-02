5eeaaba9fc48d08bf3b6e030f2000f0e392b0081
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-04-02 09:54:00 -0600

    [Removed] symlink for move_window.sh
    Now every project will have it's own move_windows.sh for specific needs

--- tmuxinator/start.sh
+++ tmuxinator/start.sh
@@ -4,16 +4,10 @@ folderpath=$(pwd)
 
 touch ./.gitignore
 
-echo move_windows.sh >> ./.gitignore
-
 tmuxinatorpath="$HOME/dotfiles/tmuxinator"
 
 cp -r "$tmuxinatorpath" "$folderpath"
 
 echo "Copied tmuxinator folder to $folderpath"
-
-rm "$folderpath/tmuxinator/move_windows.sh"
-
-ln -s "$tmuxinatorpath/tmuxinator/move_windows.sh" "$folderpath/tmuxinator/move_windows.sh"
 
 rm "$folderpath/tmuxinator/start.sh"
