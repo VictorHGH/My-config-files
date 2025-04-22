ed15d3e7f42e2153e314ad3bd56c0459132e9548
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-04-22 00:20:40 -0600

    [Updated] config for lsp-format.lua

--- global/.config/nvim/lua/user/LSP/LSP-format.lua
+++ global/.config/nvim/lua/user/LSP/LSP-format.lua
@@ -1,8 +1,37 @@
+-- Configuración general de LSP y diagnóstico en Neovim
+
 local lsp_format = require("lsp-format")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
 local lspconfig = require("lspconfig")
 
+-- Habilitar soporte para snippets
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- Configuración global del sistema de diagnósticos
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●", -- Cambia si quieres otro ícono aquí
+	},
+	signs = false,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
+
+-- Mostrar tooltip automático al dejar el cursor sobre un error
+vim.o.updatetime = 3000
+vim.cmd([[
+  autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Lista de servidores LSP con sus configuraciones
 local servers = {
 	astro = {},
 	bashls = {},
@@ -10,19 +39,13 @@ local servers = {
 	cssls = {},
 	emmet_ls = {
 		filetypes = {
-			"html",
-			"css",
-			"scss",
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"php"
+			"html", "css", "scss",
+			"javascript", "javascriptreact",
+			"typescript", "typescriptreact", "php",
 		}
 	},
 	html = {
 		filetypes = { "html" },
-		-- setting for indentInnerHtml:
 		settings = {
 			html = {
 				format = {
@@ -31,7 +54,6 @@ local servers = {
 			}
 		}
 	},
-	-- phpactor con innnerHtml
 	phpactor = {
 		format = {
 			indent_inner_html = true
@@ -45,16 +67,27 @@ local servers = {
 	ts_ls = {},
 }
 
+-- Setup de cada servidor
 for server, config in pairs(servers) do
-	config.on_attach = function(client)
-		lsp_format.on_attach(client)
+	config.on_attach = function(client, bufnr)
+		-- Habilita lsp-format
+		lsp_format.on_attach(client, bufnr)
+
+		-- Formateo automático al guardar
+		if client.server_capabilities.documentFormattingProvider then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+
+		-- keymaps por buffer se definen aquí
 	end
+
 	config.capabilities = capabilities
 	lspconfig[server].setup(config)
 end
 
-local signs = { Error = "X", Warn = " ", Hint = "!", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.diagnostic.config()
-end
