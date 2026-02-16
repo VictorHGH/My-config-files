46f8c3a567d1101bd5b2a5834e7c77884ed59616
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2026-02-16 12:20:30 -0600

    [Removed] nvim folder for only one configuration for neovim

--- macbook_pro/.config/nvim/lsp/phpactor.lua
+++
@@ -1,6 +0,0 @@
-local capabilities = require("user.LSP.common").capabilities
-
-return {
-	capabilities = capabilities,
-	format = { indent_inner_html = true },
-}
--- macbook_pro/.config/nvim/lua/user/my_functions.lua
+++
@@ -1,7 +0,0 @@
--- function to upgrade nvim running this commands: 'lazy update' and 'TSUpdate'
-local function upgrade_nvim()
-	require("lazy").update()
-	vim.cmd("MasonUpdate")
-end
-
-vim.api.nvim_create_user_command("UpgradeNvim", upgrade_nvim, {})
