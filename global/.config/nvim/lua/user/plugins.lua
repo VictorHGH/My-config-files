fd53279a0d150a98a5955cd1e3c15f3fefd4ec7a
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2026-02-16 10:55:23 -0600

    [Fixed] problem of config for treesetter

--- global/.config/nvim/lua/user/plugins.lua
+++ global/.config/nvim/lua/user/plugins.lua
@@ -62,7 +62,7 @@ local plugins = {
 	"nvim-telescope/telescope.nvim",
 
 	-- Tresitter
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "nvim-treesitter/nvim-treesitter", branch = "main", build = ":TSUpdate"},
 	"JoosepAlviste/nvim-ts-context-commentstring",
 	"wuelnerdotexe/vim-astro",
 
